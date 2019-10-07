e4746eea80ebd41693cc92aa67a74d88236db11f
Breno Antunes <p000bantunes@prservicos.com.br>
2019-10-07 18:37:55 -0300

    Removed unnecessary css, fix unit test mock and update package.json version

--- libs/sn-branch-locator/src/lib/components/branch-locator/branch-locator.service.spec.ts
+++ libs/sn-branch-locator/src/lib/components/branch-locator/branch-locator.service.spec.ts
@@ -2,6 +2,19 @@ import { TestBed, async, inject } from '@angular/core/testing';
 
 import { BranchLocatorService } from './branch-locator.service';
 
+
+const position: Position = {
+  coords: {
+    accuracy: 1,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    latitude: 123123,
+    longitude: 123,
+    speed: null
+  }, timestamp: 1234567890
+};
+
 describe('BranchLocatorService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
@@ -12,17 +25,6 @@ describe('BranchLocatorService', () => {
 
   it('When watchPosition is called Then return an observable Position', async(
     inject([BranchLocatorService], (nameService) => {
-      const position: Position = {
-        coords: {
-          accuracy: 1,
-          altitude: null,
-          altitudeAccuracy: null,
-          heading: null,
-          latitude: 123123,
-          longitude: 123,
-          speed: null
-        }, timestamp: 1234567890
-      };
 
       spyOn(navigator.geolocation, 'watchPosition').and.returnValue(position);
 
@@ -32,12 +34,12 @@ describe('BranchLocatorService', () => {
     }))
   );
 
-  it('When getCurrentPosition is called Then return an observable list of strings', async(
+  it('When getCurrentPosition is called Then return an observable Position', async(
     inject([BranchLocatorService], (nameService) => {
-      spyOn(navigator.geolocation, 'getCurrentPosition').and.returnValue({coords: {lat: 123123, lng: 123}});
+      spyOn(navigator.geolocation, 'getCurrentPosition').and.returnValue(position);
 
       nameService.getCurrentPosition().subscribe((pos: Position) => {
-        expect(pos).toBeTruthy();
+        expect(pos).toBe(position);
       });
     }))
   );
