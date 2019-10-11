e5c8b50dfe534e0f6c857cab001e1ea9779eda76
Filipe Freire <51783017+FilipeFreire@users.noreply.github.com>
2019-10-11 23:44:27 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   api: {
     BranchLocator: {
       apiURL: 'https://back-weu.azurewebsites.net/branch-locator',
-      // googleApiKey: ''
+      googleApiKey: ''
     }
   }
 };
