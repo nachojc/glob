5dd5194ea027dbca8cae2bd4faeb0f5c67a455dd
Filipe Freire <AE86+FilipeFreire@users.noreply.github.com>
2019-10-14 11:17:49 +0100

    Added Translations

--- libs/sn-branch-locator/src/lib/components/branch-search-input/branch-search-input.module.ts
+++ libs/sn-branch-locator/src/lib/components/branch-search-input/branch-search-input.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconModule } from 'sn-common-lib';
 import { BranchSearchInputComponent } from './branch-search-input.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [BranchSearchInputComponent],
   imports: [
     CommonModule,
-    IconModule
+    IconModule,
+    TranslateModule
   ],
   exports: [BranchSearchInputComponent],
   providers: [
--- libs/sn-branch-locator/src/lib/components/sn-filter/sn-filter.module.ts
+++ libs/sn-branch-locator/src/lib/components/sn-filter/sn-filter.module.ts
@@ -4,6 +4,7 @@ import { FilterComponent } from './sn-filter.component';
 import { ButtonModule, CheckboxModule, IconModule } from 'sn-common-lib';
 import { FilterService } from './services/sn-filter.service';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 
@@ -14,7 +15,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ButtonModule,
     CheckboxModule,
     IconModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslateModule
   ],
   providers: [FilterService],
   exports: [FilterComponent],
