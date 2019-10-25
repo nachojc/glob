6a517dd795380140a5c03980fa01e387b3eb3022
Ignacio Jimenez <nachojc@ipsoft.es>
2019-10-25 13:59:09 +0100

    fixed .ignore

---
+++ tools/coverage/const.js
@@ -0,0 +1,24 @@
+(function () {
+  let project;
+  const constants = (pro) => {
+    
+    project = pro || project;
+    return {
+      project: project,
+      buildFile: './.buildstatus',
+      karmaFile: `libs/${project}/karma.conf.js`,
+      getBuildUrl: getBuildUrl ,
+      getCoverageUrl: (coverage, color) => 
+        getBuildUrl('Coverage', coverage + encodeURI('%'), color),
+      jsonCoverageFile: `coverage/${project}/coverage-summary.json`,
+      readmeFile: 'README.md'
+    };
+  };
+  
+  
+    function getBuildUrl(left, right, color) {
+      return `https://img.shields.io/badge/${left}-${right}-${color}.svg`;
+    }
+    
+    module.exports = constants;
+}());
