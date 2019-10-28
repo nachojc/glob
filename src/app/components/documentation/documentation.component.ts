88444abce3232966afae1e02d9d52f133e16039b
Breno Antunes <p000bantunes@prservicos.com.br>
2019-10-28 14:57:58 -0300

    Added outputs and css to documentation

--- src/app/components/documentation/documentation.component.ts
+++ src/app/components/documentation/documentation.component.ts
@@ -7,49 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentationComponent implements OnInit {
   title = 'Branch Locator';
+  externalDependencies = `npm install @agm/core --save`;
   dependencies = `
+  import { SnBranchLocatorModule} from 'sn-branch-locator';
+  import { AgmCoreModule } from '@agm/core';
+
+  imports: [
+    ...
+    SnBranchLocatorModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.api.BranchLocator.googleApiKey,
+      libraries: environment.api.BranchLocator.googleApiLibs || []
+    }),
+    ...
+  ],
   providers: [
     ...
     { provide: ENV_CONFIG, useValue: environment as EnvironmentConfigModel },
-    { provide: 'WINDOW', useValue: window }
+    { provide: 'WINDOW', useValue: window },
     ...
-   ],
+  ],
    `;
-
-   imports = `
-   imports: [
-     ...
-   SnBranchLocatorModule,
-   AgmCoreModule.forRoot({
-     apiKey: environment.api.BranchLocator.googleApiKey,
-     libraries: environment.api.BranchLocator.googleApiLibs || []
-   }),
-   ...
- ],
- `;
 
  enviroment = `
  export const environment = {
-  ...
-  api: {
     ...
-    BranchLocator: {
-      apiURL: 'your api service url',
-      googleApiKey: 'Google Api Key here',
-      googleApiLibs: ['places'],
-      languages: './'
+    api: {
+      ...
+      BranchLocator: {
+        apiURL: 'your api service url',
+        googleApiKey: 'Google Api Key here',
+        googleApiLibs: ['places'],
+        languages: './'
+      }
+      ...
     }
-    ...
-  }
-};
+  };
  `;
 
-   example = `
+  example = `
    ...
    <sn-branch-locator></sn-branch-locator>
    ...
-   `;
+  `;
+  outputs = `<sn-branch-locator  (markerSelected)="markerSelected($event)" (mapBounds)="mapBounds($event)"></sn-branch-locator>`;
 
+  outputExample1 = `
+  markerSelected(event: OutputMarkerSelected) {
+    OutputMarkerSelected = {
+      marker: Details about the selected Marker,
+      userPosition: User Position in LatLng
+  }
+  `;
+
+  outputExample2 = `
+  mapBounds(event: OutputMapBounds) {
+    OutputMapBounds = {
+      northEast: LatLngLiteral;
+      southWest: LatLngLiteral;
+  }
+  `;
 
   constructor() { }
 
