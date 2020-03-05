df9621eda78b9255f8aad32863e3b5d1f36c9742
AlvaroLavinGFT <alvaro.lavin@gft.com>
2020-03-05 16:59:09 +0100

    sn branch locator bl group injection

--- projects/BL-group/src/environments/environment.ts
+++ projects/BL-group/src/environments/environment.ts
@@ -3,7 +3,28 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: {
+    BranchLocator: {
+      endpoints: [
+        {
+          URL: 'https://back-scus.azurewebsites.net/branch-locator',
+          lat: 29.4247,
+          lng: -98.4935
+        },
+        {
+          URL: 'https://back-weu.azurewebsites.net/branch-locator',
+          lat: 52.35,
+          lng: 4.9167
+        },
+      ],
+      // googleApiKey: 'AIzaSyAW6Ayoy5LzilIIwr84WfQFTybSV8P9Ifo',
+      googleApiKey: 'AIzaSyAEa5DdaHqV_b-40ErddBoWfEuopdvPK7I',
+      googleApiLibs: ['weather', 'geometry', 'visualization', 'places'],
+      languages: '/i18n/branchlocator/',
+      hasFilters: true,
+    }
+  }
 };
 
 /*
