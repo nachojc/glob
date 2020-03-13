25e305f287ad49f626e8200c925b2af21f733a15
Nacho <mac_pilot_globile17@MacBook-Pro-de-MAC-PILOT-GLOBILE17.local>
2020-03-13 18:39:19 +0000

    update dependencies

--- libs/sn-branch-locator/src/public-api.ts
+++ libs/sn-branch-locator/src/public-api.ts
@@ -3,7 +3,4 @@
  */
 
 export * from './lib/services/branch-locator/branch-locator.service';
-export * from './lib/components/branch-locator/sn-branch-locator.component';
 export * from './lib/sn-branch-locator.module';
-export * from './lib/models/branch.model';
-export * from './lib/models/routes.model';
