8228e36f62f4f73552abe386b0c33c2e4dca18a1
Isidoro Javier Pérez Megia <javier.perezmegia@servexternos.gruposantander.com>
2020-03-18 11:26:40 +0100

    WIP

---
+++ libs/sn-branch-locator/src/lib/constants/events-analytics-variables.ts
@@ -0,0 +1,11 @@
+export const EventsAnalyticsVariables: any = {
+  sendMbw: {
+    clickSearchContact: {
+      ScreenName: 'payments:step1',
+      EventCategory: 'payments action',
+      EventAction: 'select contact',
+      EventLabel: '<suggested|address book>',
+      EventName: 'payments actionWIP'
+    }
+  }
+};
---
+++ libs/sn-branch-locator/src/lib/constants/views-analytics-variables.ts
@@ -0,0 +1,9 @@
+export const ViewsAnalyticsVariables: any = {
+  successScreen: {
+    ScreenName: 'payments:success',
+    ProcessName: 'payments',
+    AmountStage: '<0-100|101-200|201-300|301-400| etc...>',
+    AmountPaid: '<#amountpaid>',
+    AmountFee: '<#amountfee>WIP'
+  }
+};
