2bf617385c245b675e2474f98aa4efea56fc8c91
Isidoro Javier Pérez Megia <javier.perezmegia@servexternos.gruposantander.com>
2020-03-19 14:06:48 +0100

    events analytics variables reviewed & created

--- libs/sn-branch-locator/src/lib/constants/events-analytics-variables.ts
+++ libs/sn-branch-locator/src/lib/constants/events-analytics-variables.ts
@@ -1,11 +1,59 @@
 export const EventsAnalyticsVariables: any = {
   sendMbw: {
-    clickSearchContact: {
-      ScreenName: 'payments:step1',
-      EventCategory: 'payments action',
-      EventAction: 'select contact',
-      EventLabel: '<suggested|address book>',
-      EventName: 'payments actionWIP'
+    clickSearchBar: {
+      ScreenName: 'branch locator:home',
+      EventAction: 'tap search bar',
+      EventName: 'tap search bar'
+    },
+    clickFilters: {
+      ScreenName: 'branch locator:home',
+      EventAction: 'tap filters',
+      EventName: 'tap filters'
+    },
+    clickBranchDetails: {
+      ScreenName: 'branch locator:home',
+      EventAction: 'tap branch detail',
+      EventName: 'tap branch detail'
+    },
+    clickTabPicker: {
+      ScreenName: 'branch locator:home',
+      EventAction: 'tap branch picker',
+      EventName: 'tap branch picker'
+    },
+    clickBranchIcon: {
+      ScreenName: 'branch locator:home',
+      EventAction: 'tap branch icon',
+      EventName: 'tap branch icon'
+    },
+    clickCall: {
+      ScreenName: 'branch locator:home',
+      EventAction: 'tap on call button',
+      EventName: 'tap on call button'
+    },
+    performSearch: {
+      ScreenName: 'branch locator:home',
+      EventAction: 'search',
+      EventName: 'search',
+      TermSearched: ''
+    },
+    tapSearchResult: {
+      ScreenName: 'branch locator:home',
+      EventAction: 'tap search result',
+      EventName: 'tap search result',
+      TermSearched: '',
+      NSearchResult: '',
+      ClickedResult: '',
+    },
+    tapCleanFilters: {
+      ScreenName: 'branch locator:filters',
+      EventAction: 'tap clean filters',
+      EventName: 'tap clean filters'
+    },
+    tapApplyFilters: {
+      ScreenName: 'branch locator:filters',
+      EventAction: 'tap apply filters',
+      FilterSelected: '',
+      EventName: 'tap apply filters'
     }
   }
 };
