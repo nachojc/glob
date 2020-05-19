862fe5a868f37b34f6c7f4e06e33e853df8fe60b
Dominik <dcabos@trilogyinternational.com>
2020-05-19 18:54:43 +0200

    Rearranged option filters within i18n.

--- libs/sn-branch-locator/src/lib/components/menu/menu.component.ts
+++ libs/sn-branch-locator/src/lib/components/menu/menu.component.ts
@@ -12,20 +12,20 @@ import {animate, style, transition, trigger} from '@angular/animations';
   animations: [MenuAnimations.menuTrigger, trigger('slideState', [
     transition('*=>true', [
       style({transform: 'translateX(100%)'}),
-      animate('2000ms ease-in', style({transform: 'translateX(0%)'}))
+      animate('400ms ease-out', style({transform: 'translateX(0%)'}))
     ]),
     transition('true=>*', [
       style({display: 'block'}),
-      animate('2000ms ease-in', style({transform: 'translateX(100%)'}))
+      animate('400ms ease-out', style({transform: 'translateX(100%)'}))
     ])
   ]),
     trigger('slideStateOut', [
       transition('* => true', [
-        animate('2000ms ease-in', style({transform: 'translateX(-100%)'})),
+        animate('400ms ease-out', style({transform: 'translateX(-100%)'})),
       ]),
       transition('true => *', [
         style({transform: 'translateX(-100%)'}),
-        animate('2000ms ease-in', style({transform: 'translateX(0%)'})),
+        animate('400ms ease-out', style({transform: 'translateX(0%)'})),
       ])
     ])]
 })
