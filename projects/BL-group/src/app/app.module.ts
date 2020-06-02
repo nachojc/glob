dab59268ba4d90a6651ec1e1e3f7656ac8daa3e5
Nacho <mac_pilot_globile17@macpilolobile17.home>
2020-06-02 16:33:34 +0100

    bl group

--- projects/BL-group/src/app/app.module.ts
+++ projects/BL-group/src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF, CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SnBranchLocatorModule } from '@globile/branch-locator';
 import { AgmCoreModule } from '@agm/core';
 import { environment } from '../environments/environment';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PrismModule } from './components/prism/prism.module';
-import { RouterModule } from '@angular/router';
-import { GlobileSettingsService, GlobileModule } from '@globile/mobile-services';
-
-export function HttpLoaderFactory(http: HttpClient, globileSettings: GlobileSettingsService) {
-  return new TranslateHttpLoader(http, globileSettings.branchLocator.languages, '.json');
-}
 
 @NgModule({
   declarations: [
@@ -29,20 +20,10 @@ export function HttpLoaderFactory(http: HttpClient, globileSettings: GlobileSett
     HttpClientModule,
     ReactiveFormsModule,
     SnBranchLocatorModule,
-    PrismModule,
     AgmCoreModule.forRoot({
       apiKey: environment.branchLocator.googleApiKey,
       libraries: environment.branchLocator.googleApiLibs || []
     }),
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient, GlobileSettingsService]
-      }
-    }),
-    RouterModule.forRoot([]),
-    GlobileModule.forRoot({}, environment),
   ],
   providers: [
     { provide: 'WINDOW', useValue: window },
