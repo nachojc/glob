3e83958426ad4d847b1ac2b9181671bca425513d
Dominik <dcabos@trilogyinternational.com>
2020-06-04 16:42:57 +0200

    Restored app routing with generic router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {DocumentationComponent} from './components/documentation/documentation.c
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ReactiveFormsModule} from '@angular/forms';
-import {GlobileModule} from '@globile/mobile-services';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
@@ -33,27 +33,30 @@ import {GlobileModule} from '@globile/mobile-services';
       libraries: environment.branchLocator.googleApiLibs || []
     }),
     PrismModule,
-    GlobileModule.forRoot({
-      modulesRoutes: [
-        {
-          path: 'documentation',
-          component: DocumentationComponent
-        },
-        {
-          path: 'sample', // TODO: Use ModuleId.BRANCHLOCATOR from globile_services
-          loadChildren: () => import('./modules/branch-locator/branch-locator-wrapper.module').then(m => m.BranchLocatorModule)
-        }],
-      nativeModules: [],
-      notFoundRoute: 'documentation',
-      startRoute: 'documentation'
-    }, environment),
+    RouterModule.forRoot([
+      {
+        path: 'documentation',
+        component: DocumentationComponent
+      },
+      {
+        path: 'sample', // TODO: Use ModuleId.BRANCHLOCATOR from globile_services
+        loadChildren: () => import('./modules/branch-locator/branch-locator-wrapper.module').then(m => m.BranchLocatorModule)
+      },
+      {
+        path: '',
+        redirectTo: '/documentation',
+        pathMatch: 'full'
+      },
+      {
+        path: '**', component: DocumentationComponent
+      }
+    ])
   ],
-  providers: [
+providers: [
     { provide: 'WINDOW', useValue: window },
     { provide: 'ENV_CONFIG', useValue: environment },
     { provide: APP_BASE_HREF, useValue: './' },
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-
   ],
   bootstrap: [AppComponent]
 })
